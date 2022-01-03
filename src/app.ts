cb5200c5acbe486949f6b24ad76f3d5341c5d3e9
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-03 23:31:52 +0900

    126 再利用可能なバリデーション機能の作成

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+// Validation
+interface Validateble {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validateble) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === "number"
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // Autobind decorator
 function Autobind(
   target: any,
@@ -56,10 +100,27 @@ class ProjectInput {
     const enterdDescription = this.descriptionInputElement.value;
     const enterdManday = this.mandayInputElement.value;
 
+    const titleValidatable: Validateble = {
+      value: enterdTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validateble = {
+      value: enterdDescription,
+      required: true,
+      minLength: 5,
+    };
+
+    const mandayValidatable: Validateble = {
+      value: +enterdManday,
+      required: true,
+      min: 1,
+      max: 1000,
+    };
+
     if (
-      enterdTitle.trim().length === 0 ||
-      enterdDescription.trim().length === 0 ||
-      enterdManday.trim().length === 0
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(mandayValidatable)
     ) {
       alert("入力値が正しくありません。");
     } else {
